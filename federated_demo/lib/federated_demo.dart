02f30786305683d494a129c3efcd2e856d79659a
Dillon Nys <nydillon@amazon.com>
2021-05-25 15:33:18 -0700

    Add dart registry changes

--- federated_demo/lib/federated_demo.dart
+++ federated_demo/lib/federated_demo.dart
@@ -1,20 +1,6 @@
-import 'dart:io';
-
-import 'package:federated_demo_dart/federated_demo_dart.dart';
 import 'package:federated_demo_platform_interface/federated_demo_platform_interface.dart';
-import 'package:flutter/foundation.dart';
 
 class FederatedDemo implements FederatedDemoPlatform {
-  static final _instance = FederatedDemo._();
-  FederatedDemo._() {
-    if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
-      return;
-    }
-    FederatedDemoPlatform.instance = FederatedDemoDart();
-  }
-
-  factory FederatedDemo() => _instance;
-
   @override
   Future<String> getPlatform() {
     return FederatedDemoPlatform.instance.getPlatform();
--- federated_demo_dart/lib/federated_demo_dart.dart
+++ federated_demo_dart/lib/federated_demo_dart.dart
@@ -5,6 +5,12 @@ import 'dart:io';
 import 'package:federated_demo_platform_interface/federated_demo_platform_interface.dart';
 
 class FederatedDemoDart extends FederatedDemoPlatform {
+  static final instance = FederatedDemoDart();
+
+  static void registerWith() {
+    FederatedDemoPlatform.instance = instance;
+  }
+
   @override
   Future<String> getPlatform() async => Platform.operatingSystem;
 }
